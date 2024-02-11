f48f33677b65bab7872872eb4a723916c9f99308
pavelbekarev <bekarev.ps@gmail.com>
2024-02-11 19:35:41 +0500

    added gallery and load button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './styles/App.scss';
 import Header from './components/Header';
+import Slider from './components/Slider';
 
 function App() {
   return (
-    <section className='section'>
-      <div className='container'>
-        <Header />
-      </div>
-    </section>
+    <>
+      <section className='section'>
+        <div className='container'>
+          <Header />
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <Slider />
+        </div>
+      </section>
+    </>
   );
 }
 
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import "../styles/Slider.scss";
-
+import axios from "axios";
+import ArrowLeft from "./ArrowLeft";
+import ArrowRight from "./ArrowRight";
 
 const Slider = () => {
+    const url1 = "https://api.unsplash.com/photos/random/?client_id=W_svICIqdwVBQnQ3RwP57C4s6B1U4vxpg7rOHg_PRrY&count=4";
+    const [character, setCharacter] = useState<any[]>([]);
+    const [img, setImg] = useState<any>();
+
+    useEffect(() => {
+        axios.get(url1).then(res => {
+            setCharacter(res.data);
+        })
+    }, [])
+
     return (
         <div className="slider__wrapper">
-            
+            <div className="slider__items">
+                {
+                    character && character.map((el) => {
+                        return (
+                            <div className="slider__item">
+                                <img key={el.user.id} src={el.urls.regular} alt="" />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+            <div className="load-button">
+                <a href="">Загрузить</a>
+            </div>
         </div>
     );
 }
